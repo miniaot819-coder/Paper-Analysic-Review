27ff3d2bfce7b78585bcce6cf6ea97ff88dc027c
Son2110 <lyhoangson21102005@gmail.com>
2026-07-17 14:09:14 +0700

    chore: minimize CodeRabbit review scope

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -35,12 +35,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        testInstrumentationRunner = "pl.leancode.patrol.PatrolJUnitRunner"
-        testInstrumentationRunnerArguments["clearPackageData"] = "true"
-    }
-
-    testOptions {
-        execution = "ANDROIDX_TEST_ORCHESTRATOR"
     }
 
     buildTypes {
@@ -54,8 +48,4 @@ android {
 
 flutter {
     source = "../.."
-}
-
-dependencies {
-    androidTestUtil("androidx.test:orchestrator:1.5.1")
 }
